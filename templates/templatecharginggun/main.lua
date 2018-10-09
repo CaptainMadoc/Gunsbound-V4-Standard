a854c71da9a98e72d56147cde789f8610a5d4a2c
RexmecK <rexmeck@gmail.com>
2018-10-08 22:40:25 -0400

    charging gun done

--- templates/templatecharginggun/main.lua
+++ templates/templatecharginggun/main.lua
@@ -6,7 +6,7 @@ main = {
     reloadLoop = false,
     semifired = false,
     charge = false,
-    chargeDamageMultiplier = 0
+    charged = false
 }
 
 --Callbacks 
@@ -39,7 +39,7 @@ function main:update(dt, fireMode, shiftHeld, moves)
         self:updateQueuedFire(fireMode) 
         self:updateSpecial(shiftHeld, moves.up)
     else
-        if not animation:isAnyPlaying() then
+        if not animation:isAnyPlaying() then --end reload
             if not magazine:playerHasAmmo() or magazine:count() == magazine.size then
                 self.reloadLoop = false
                 self:animate(data.gunAnimations["reloadEnd"])
@@ -51,13 +51,17 @@ function main:update(dt, fireMode, shiftHeld, moves)
 end
 
 --this is after init (next frame)
-function main:lateinit() end
+--function main:lateinit() end
 
 --this is called when the lua state is being destroyed
-function main:uninit() end
+--function main:uninit() end
 
 
 --OTHER FUNCTIONS
+
+function main:initLocalUI()
+
+end
 
 --We made this so we dont deal with manually shoot + _dry animations
 function main:animate(type,noprefix)
@@ -68,18 +72,22 @@ function main:animate(type,noprefix)
     end
 end
 --We made this so we dont deal with manually shoot + _dry animations
-function main:unanimate(type,noprefix)
+function main:unAnimate(type,noprefix)
     if not noprefix and (gun:chamberDry() and (not gun.hasToLoad and not data.bypassShellEject)) then
-		animation:stop(data.gunAnimations[type.."_dry"] or data.gunAnimations[type] or type.."_dry")
+		animation:skip(data.gunAnimations[type.."_dry"] or data.gunAnimations[type] or type.."_dry")
 	else
-		animation:stop(data.gunAnimations[type] or type)
+		animation:skip(data.gunAnimations[type] or type)
     end
 end
 
+function main:isAnimate(type)
+    return animation:isPlaying(data.gunAnimations[type]) or animation:isPlaying(data.gunAnimations[type.."_dry"]) or animation:isPlaying(type.."_dry")
+end
+
 --Fire implementation
-function main:fire()
+function main:fire(overrideStats)
     if not animation:isAnyPlaying() or animation:isPlaying({data.gunAnimations["shoot"], data.gunAnimations["shoot_dry"]}) then
-        local status = gun:fire()
+        local status = gun:fire(overrideStats)
         if status then
             self:animate("shoot")
         else
@@ -90,7 +98,7 @@ end
 
 --Specials like reload or lever switches stuff and things
 function main:updateSpecial(shift, up)
-    if shift and up and not animation:isAnyPlaying() thenlf
+    if shift and up and not animation:isAnyPlaying() then
         self:animate("reload")
     end
     if not shift and up and not animation:isAnyPlaying() then
@@ -104,60 +112,135 @@ end
 
 --we use this for deal with gun common firemodes.
 function main:updateFire(firemode)
-    if firemode == "primary" and gun:ready() and not self.charge then
+    if firemode == "primary" and gun:ready() and not self.charge then --init charge
         self.charge = 0
         self:animate("charge")
-    elseif firemode == "primary" and gun:ready() and self.charge < data.chargedTime then
+        if animator.hasSound("charge") then
+            animator.playSound("charge")
+        end
+    elseif firemode == "primary" and gun:ready() and self.charge < data.chargedTime then --charging loop
         self.charge = math.max(math.min(updateInfo.dt + self.charge, data.chargedTime), 0)
-        if animation:isAnyPlaying() then
+
+        if not self:isAnimate("charge_loop") and not self:isAnimate("charge") then
+
             self:animate("charge_loop")
+
+            if animator.hasSound("charge") then
+                animator.stopAllSounds("charge")
+            end
+
+            if animator.hasSound("charging") then
+                animator.stopAllSounds("charging")
+                animator.playSound("charging")
+            end
+
         end
-    elseif firemode == "primary" and self.charge == data.chargedTime then
-        if animation:isAnyPlaying() then
-            self:unanimate("charge_loop")
+
+    elseif firemode == "primary" and self.charge == data.chargedTime and not self.charged then --init charged
+        if self:isAnimate("charge_loop") then
+            self:unAnimate("charge_loop")
         end
+
+        if animator.hasSound("charging") then
+            animator.stopAllSounds("charging")
+        end
+        
+        if animator.hasSound("charged") then
+            animator.playSound("charged")
+        end
+
         self:animate("charged")
-    elseif firemode ~= "primary" and self.charge then
-        self:unanimate("charge")
-        self:unanimate("charge_loop")
+        self.charged = true
+    elseif firemode == "primary" and self.charge == data.chargedTime and self.charged then -- charged
+        --just empty
+    elseif firemode ~= "primary" and self.charge then --fire charge
+
+        if self:isAnimate("charge") then
+            self:unAnimate("charge")
+        end
+
+        if self:isAnimate("charge_loop") then
+            self:unAnimate("charge_loop")
+        end
+
+        if self:isAnimate("charged") then
+            self:unAnimate("charged")
+        end
+
+        self.charged = false
+
         if self.charge == 0 then
 
         else
+            --fire queue
             if gun:fireMode() == "burst" then
                 self.fireQueued = data.gunStats.burst or 3
             else
                 self.fireQueued = 1
             end
-            self.chargeDamageMultiplier = self:percentDamage(data.gunStats.damageMultiplier, data.gunStats.chargedDamageMultiplier, self.charge)
+            -- original stats -> charged Stats Ratio
+            self.chargeRatio = self.charge / data.chargedTime
         end
         self.charge = false
     end
 end
 
+--returns a gunStat
+function main:chargedStatsRatio(gunstats, chargedstats, ratio)
+    local finalStats = {}
+    for i,v in pairs(gunstats) do
+        if type(chargedstats[i]) == "number" then --float
+            finalStats[i] = v + (chargedstats[i] - v) * ratio
+        elseif type(chargedstats[i]) == "table" and #chargedstats[i] == 2 and type(v) == "table" and #v == 2 then --vec2 why
+            finalStats[i] = {}
+            finalStats[i][1] = v[1] + (chargedstats[i][1] - v[1]) * ratio
+            finalStats[i][2] = v[2] + (chargedstats[i][2] - v[2]) * ratio
+        else
+            finalStats[i] = v
+        end
+    end
+
+    return finalStats
+end
+
+-- we're using this for calculting changes from the attachment stats
+function main:statsAdd(stats, statsadd) 
+    if not statsadd then statsadd = {} end
+    local ret = {}
+    for i,v in pairs(stats or {}) do
+        if type(statsadd[i]) == "number" then
+            ret[i] = v + statsadd[i]
+        elseif type(statsadd[i]) == "table" and #statsadd[i] == 2 and type(v) == "table" and #v == 2 then
+            ret[i] = {}
+            ret[i][1] = v[1] + statsadd[i][1]
+            ret[i][2] = v[2] + statsadd[i][2]
+        else
+            ret[i] = v
+        end
+    end
+    return ret
+end
+
  -- our queued burst fires
 function main:updateQueuedFire()
+
     local gunFireMode = gun:fireMode() -- we get his firemode
+
     if self.fireQueued > 0 and gun:ready() and not gun:chamberDry() then
-        local fireStatus = self:fire({power = self.chargeDamageMultiplier})
-        if magazine:count() == 0 then
+        local fireStatus = self:fire(self:chargedStatsRatio(data.gunStats, self:statsAdd(data.chargedGunStats, attachment), self.chargeRatio))
+
+        if magazine:count() == 0 then -- if empty, we cancel burst
             self.fireQueued = 0
         else
             self.fireQueued = self.fireQueued - 1
         end
 
-        if self.fireQueued == 0 then
+        if self.fireQueued == 0 then -- queue finished reset
             gun.cooldown = gun:rpm()
-            self.chargeDamageMultiplier = 0
+            self.chargeRatio = 0
         end
     end
 end
-
---do not use this i think we have the auto chamber load from the internal
---function main:updateChamber()
---    if gun:ready() and gun:chamberDry() and magazine:count() > 0 and (not animation:isAnyPlaying() or animation:isPlaying({data.gunAnimations["shoot_dry"], data.gunAnimations["shoot"]}) ) then
---        gun:load_chamber()
---    end
---end
 
 --smart automatic reload 
 function main:updateAutoReload()
